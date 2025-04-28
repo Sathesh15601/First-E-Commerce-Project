b4ae74b2cd345451279fe84e6d1a3ca1265b9d4a
SATHESH KUMAR B <Sathesh15601@gmail.com>
2025-04-28 16:25:11 +0530

    total cart amount components  and check out page added

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "../components/Title";
 
 const Orders = () => {
-  return <div>Orders</div>;
+  const { porducts, currency } = useContext(ShopContext);
+  return (
+    <div className="border-t pt-16">
+      <div className="text-2xl">
+        <Title text1={"MY"} text2={"ORDER"} />
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
